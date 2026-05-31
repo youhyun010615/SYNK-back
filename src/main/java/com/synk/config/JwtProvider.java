b9f1401a5f3d44e4e62ce3ce6fa7be730e359090
유현 <cadry238@gmail.com>
2026-05-31 22:41:47 +0900

    feat(JwtProvider): 모든 API 요청이 들어올 때 토큰 꺼내서 검증 (#8)

---
+++ src/main/java/com/synk/config/JwtProvider.java
@@ -0,0 +1,60 @@
+// generateToken() : 로그인 성공 시 유저 ID를 담아 토큰 생성
+// getuserId() :  토큰에서 유저 ID 꺼내기
+// validateToken() : 토큰이 유효한지 검사 (만료/위조 확인)
+
+package com.synk.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JwtProvider {
+    private final SecretKey secretKey;
+    private final long expiration;
+
+    public JwtProvider(
+            @Value("${jwt.secret}") String secret,
+            @Value("${jwt.expiration}") long expiration) {
+        this.secretKey = Keys.hmacShaKeyFor(secret.getBytes());
+        this.expiration = expiration;
+    }
+
+    public String generateToken(Long userId) {
+        return Jwts.builder()
+                .subject(String.valueOf(userId))
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public Long getUserId(String token) {
+        Claims claims = Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+        return Long.parseLong(claims.getSubject());
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser()
+                    .verifyWith(secretKey)
+                    .build()
+                    .parseSignedClaims(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+
+}
