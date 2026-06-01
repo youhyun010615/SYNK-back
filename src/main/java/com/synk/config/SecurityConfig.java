b4fbbea3fbb35d5773adbd4955a87073c3b8a95a
유현 <cadry238@gmail.com>
2026-06-01 13:07:22 +0900

    feat(cors) : CORS 설정 (#30)

--- src/main/java/com/synk/config/SecurityConfig.java
+++ src/main/java/com/synk/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import
         org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -27,6 +28,7 @@ public class SecurityConfig {
 
     private final JwtProvider jwtProvider;
     private final UserRepository userRepository;
+    private final CorsConfigurationSource corsConfigurationSource;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
